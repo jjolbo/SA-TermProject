2e300f8c5bbb1c7fa74003bd269aaec8d9c45108
jjolbo <jeongha3751@kookmin.ac.kr>
2020-06-28 02:38:19 +0900

    refactor: connect Firebase

--- auto_lib/src/index.js
+++ auto_lib/src/index.js
@@ -4,6 +4,16 @@ import './index.css';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
+import * as firebase from 'firebase';
+
+const config = {
+    apiKey: "AIzaSyDIubT7S1mlJRwUHCELzoVOW2VHs5xkulo",
+    authDomain: "librarysystem-ce797.firebaseapp.com",
+    databaseURL: "https://librarysystem-ce797.firebaseio.com",
+    storageBucket: "librarysystem-ce797.appspot.com"
+};
+
+firebase.initializeApp(config);
 
 ReactDOM.render(
     <React.StrictMode>
@@ -14,7 +24,4 @@ ReactDOM.render(
     document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
